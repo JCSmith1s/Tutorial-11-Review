5a69001bf7cf4e49b76421d848ebf4910c0cf61c
JCSmith1s <JCSmith1s@capetigers.com>
2026-02-27 10:14:59 -0600

    final commit

--- js/jpf_hitori.js
+++ js/jpf_hitori.js
@@ -52,154 +52,195 @@ var allCells;
 window.onload = startUp;
 
 function startUp() {
-   // Insert the title for the first hitori puzzle
-   document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
+  // Insert the title for the first hitori puzzle
+  document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
 
-   // Insert the HTML code for the first puzzle table
-   document.getElementById("puzzle").innerHTML = drawHitori(hitori1Numbers, hitori1Blocks, hitori1Rating );
+  // Insert the HTML code for the first puzzle table
+  document.getElementById("puzzle").innerHTML = drawHitori(
+    hitori1Numbers,
+    hitori1Blocks,
+    hitori1Rating,
+  );
 
-   // Add event handlers for the puzzle buttons
-   var puzzleButtons = document.getElementsByClassName("puzzles");
-   for (var i = 0; i < puzzleButtons.length; i++) {
-      puzzleButtons[i].onclick = switchPuzzle;
-   }
+  // Add event handlers for the puzzle buttons
+  var puzzleButtons = document.getElementsByClassName("puzzles");
+  for (var i = 0; i < puzzleButtons.length; i++) {
+    puzzleButtons[i].onclick = switchPuzzle;
+  }
 
-      setupPuzzle();
+  setupPuzzle();
 
-            
-      
-      
-
-   document.getElementById("solve").addEventListener("click", showSolution)
-      
-      
-
+  document.getElementById("check").onclick = findErrors;
+  document.getElementById("solve").onclick = showSolution;
 }
 
 function switchPuzzle(e) {
-   if(confirm("You will lose all of your work on the puzzle! Continue?")) {
-   var puzzleID = e.target.id;
+  if (confirm("You will lose all of your work on the puzzle! Continue?")) {
+    var puzzleID = e.target.id;
 
-   var puzzleTitle = e.target.value;
-   document.getElementById("puzzleTitle").innerHTML = puzzleTitle;
+    var puzzleTitle = e.target.value;
+    document.getElementById("puzzleTitle").innerHTML = e.target.value;
 
-   switch (puzzleID) {
+    switch (puzzleID) {
       case "puzzle1":
-         document.getElementById("puzzle").innerHTML = drawHitori(hitori1Numbers, hitori1Blocks, hitori1Rating);
-         break;
+        document.getElementById("puzzle").innerHTML = drawHitori(
+          hitori1Numbers,
+          hitori1Blocks,
+          hitori1Rating,
+        );
+        break;
 
       case "puzzle2":
-         document.getElementById("puzzle").innerHTML = drawHitori(hitori2Numbers, hitori2Blocks, hitori2Rating);
-         break;
+        document.getElementById("puzzle").innerHTML = drawHitori(
+          hitori2Numbers,
+          hitori2Blocks,
+          hitori2Rating,
+        );
+        break;
 
       case "puzzle3":
-         document.getElementById("puzzle").innerHTML = drawHitori(hitori3Numbers, hitori3Blocks, hitori3Rating);
-         break;
-   }
+        document.getElementById("puzzle").innerHTML = drawHitori(
+          hitori3Numbers,
+          hitori3Blocks,
+          hitori3Rating,
+        );
+        break;
+    }
 
-   setupPuzzle();
-}
-
+    setupPuzzle();
+  }
 }
 
 function setupPuzzle() {
-   allCells = document.querySelectorAll("table#hitoriGrid td");
+  allCells = document.querySelectorAll("table#hitoriGrid td");
 
-   for (let i = 0; i < allCells.length; i++) {
-      allCells[i].style.backgroundColor = "white";
-      allCells[i].style.color = "black";
-      allCells[i].style.borderRadius = "0";
+  for (let i = 0; i < allCells.length; i++) {
+    allCells[i].style.backgroundColor = "white";
+    allCells[i].style.color = "black";
+    allCells[i].style.borderRadius = "0";
 
+    allCells[i].onmousedown = function (e) {
+      if (e.shiftKey) {
+        e.target.style.backgroundColor = "white";
+        e.target.style.color = "black";
+        e.target.style.borderRadius = "0";
+      } else if (e.altKey) {
+        e.target.style.backgroundColor = "black";
+        e.target.style.color = "white";
+        e.target.style.borderRadius = "0";
+      } else {
+        e.target.style.backgroundColor = "rgb(101, 101, 101)";
+        e.target.style.color = "white";
+        e.target.style.borderRadius = "50%";
+      }
+      e.preventDefault();
+    };
 
+    allCells[i].addEventListener("mouseover", function (e) {
+      if (e.shiftKey) {
+        e.target.style.cursor = "url(./img/jpf_eraser.png), alias";
+      } else if (e.altKey) {
+        e.target.style.cursor = "url(./img/jpf_block.png), cell";
+      } else {
+        e.target.style.cursor = "url(./img/jpf_circle.png), pointer";
+      }
+    });
 
-   }
-   
-   
-   
-   
-   
+    allCells[i].addEventListener("mouseup", checkSolution);
+  }
 }
 
+/* findErrors() function to highlight incorrect tiles red when incorrect */
+function findErrors() {
+  for (var i = 0; i < allCells.length; i++) {
+    if ((allCells[i].className === "blocks" && allCells[i].style.backgroundColor === "rgb(101, 101, 101)")
+    ||
+    (allCells[i].className === "circles" && allCells[i].style.backgroundColor === "black")) {
+      allCells[i].style.color = "red";
+    }
+  }
 
+  setTimeout(function(){
+    for(var i = 0; i < allCells.length; i++) {
+      if ((allCells[i].className === "circles" && allCells[i].style.backgroundColor === "black")) {
+        allCells[i].style.color = "white";
+      }
+    }
+  }, 1000);
+}
 
-
-
-
-
-
-         
 /* ================================================================= */
 
 function checkSolution() {
-   /* Set the initial solved state of the puzzle to true */
-   var solved = true;
+  /* Set the initial solved state of the puzzle to true */
+  var solved = true;
 
-   /* Loop through the puzzle cells, exiting when an incorrect
+  /* Loop through the puzzle cells, exiting when an incorrect
       cell is found, setting the solved variable to false */
 
-   for (var i = 0; i < allCells.length; i++) {
-      var cellColor = allCells[i].style.backgroundColor;
-      var cellClass = allCells[i].className;
+  for (var i = 0; i < allCells.length; i++) {
+    var cellColor = allCells[i].style.backgroundColor;
+    var cellClass = allCells[i].className;
 
-      /* A cell is incorrect if it is in the block class and is not black
+    /* A cell is incorrect if it is in the block class and is not black
          or in the circle class and is not white */
-      if ( (cellClass == "blocks" && cellColor !== "black") || 
-           (cellClass == "circles" && cellColor !== "rgb(101, 101, 101)")) {
-         solved = false;
-         break;
-      }
-   }
+    if (
+      (cellClass == "blocks" && cellColor !== "black") ||
+      (cellClass == "circles" && cellColor !== "rgb(101, 101, 101)")
+    ) {
+      solved = false;
+      break;
+    }
+  }
 
-   /* If solved is still true after the loop, display an alert box */
-   if (solved) alert("Congratulations! You solved the puzzle!");
+  /* If solved is still true after the loop, display an alert box */
+  if (solved) alert("Congratulations! You solved the puzzle!");
 }
 
-function showSolution () {
-   for (var i = 0; i < allCells.length; i++) {
-      allCells[i].style.color = "";
-      allCells[i].style.backgroundColor = "";
-      allCells[i].style.borderRadius = "";
-   };   
+function showSolution() {
+  for (var i = 0; i < allCells.length; i++) {
+    allCells[i].style.color = "";
+    allCells[i].style.backgroundColor = "";
+    allCells[i].style.borderRadius = "";
+  }
 }
 
 function drawHitori(numbers, blocks, rating) {
+  /* Initial HTML String for the Hitori Puzzle */
+  var htmlString = "";
 
-   /* Initial HTML String for the Hitori Puzzle */
-   var htmlString = "";
-
-   /* numbers is a multidimensional array containing the
+  /* numbers is a multidimensional array containing the
       Hitori numbers; blocks is a corresponding 
       multidimensional array containing the location of the
       blocks which are indicated by the # character.
       Non-blocking cells are indicated by a blank character.
   */
 
-   /* Create a Web table with the id, hitoriGrid, containing
+  /* Create a Web table with the id, hitoriGrid, containing
       the numeric values. Blocks cells have the class name,
       blocks. Non-blocking cells have the class name, circles
   */
 
-   var totalRows = numbers.length;
-   var totalCols = numbers[0].length;
-   htmlString = "<table id='hitoriGrid'>";
-   htmlString += "<caption>" + rating + "</caption>";
-   
+  var totalRows = numbers.length;
+  var totalCols = numbers[0].length;
+  htmlString = "<table id='hitoriGrid'>";
+  htmlString += "<caption>" + rating + "</caption>";
 
-   for (var i = 0; i < totalRows; i++) {
-      htmlString += "<tr>";
+  for (var i = 0; i < totalRows; i++) {
+    htmlString += "<tr>";
 
-      for (var j = 0; j < totalCols; j++) {
-         if (blocks[i][j] == "#") htmlString += "<td  class='blocks'>"
-         else htmlString += "<td class='circles'>";
+    for (var j = 0; j < totalCols; j++) {
+      if (blocks[i][j] == "#") htmlString += "<td  class='blocks'>";
+      else htmlString += "<td class='circles'>";
 
-         htmlString += numbers[i][j];
-         htmlString +="</td>";
-      }
+      htmlString += numbers[i][j];
+      htmlString += "</td>";
+    }
 
-      htmlString += "</tr>";
-   }
+    htmlString += "</tr>";
+  }
 
-   htmlString += "</table>";
+  htmlString += "</table>";
 
-   return htmlString;
+  return htmlString;
 }
